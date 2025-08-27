07c313a8cf712634f18b4d3e7ac3969adb5c26d2
ykuchiki <ykuchiki0827@gmail.com>
2025-08-28 02:13:48 +0900

    fix path

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,6 @@ export default {
   reactStrictMode: true,
   output: 'export',
   trailingSlash: true,
-  basePath: '/homepage',
-  assetPrefix: '/homepage',
   images: {
     unoptimized: true
   }
